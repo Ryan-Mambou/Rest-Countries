fb6677486a2bdb7270d1b0a4e99b6d04b897c917
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-09-20 10:12:24 +0100

    image sizing

--- components/CountryCard.jsx
+++ components/CountryCard.jsx
@@ -6,8 +6,8 @@ function CountryCard({ country }) {
   console.log(country)
   return (
     <div className='bg-white shadow-lg  rounded-md overflow-hidden h-[25rem] cursor-pointer'>
-        <img src={country.flags.png} alt='Country card'/>
-        <span className='px-3 py-2 block font-bold text-xl'>{country.name.common}</span>
+        <img src={country.flags.png} alt='Country card h-40 w-full object-cover'/>
+        <span className='px-3 py-2 block font-bold text-xl '>{country.name.common}</span>
         <span className='px-3 py-2 block'><span className='font-bold'>Popoulation:</span> <span className='font-light'>{country.population}</span></span>
         <span className='px-3 py-2 block'><span className='font-bold'>Region:</span> <span className='font-light'>{country.region}</span></span>
         <span className='px-3 py-2 block'><span className='font-bold'>Capital:</span> <span className='font-light'>{country.capital}</span></span>
