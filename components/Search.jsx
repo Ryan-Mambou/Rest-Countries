4bb0a6e20fa2c71e09cb25d6e27685cc2b161299
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-09-21 16:51:55 +0100

    fixing searchbar style

--- components/Search.jsx
+++ components/Search.jsx
@@ -4,8 +4,8 @@ import { IoSearchSharp } from "react-icons/io5";
 function Search() {
   return (
     <div className='relative '>
-    <input placeholder='Search for a country...' className='text-sm py-3 w-[30rem] rounded placeholder:ml-10 shadow-lg focus:outline-none'/>
-    <IoSearchSharp className='absolute bottom-4 left-3 text-gray-400 text-2xl'/>
+    <input placeholder='Search for a country...' className='text-sm py-3 w-[30rem] rounded pl-10 shadow-lg focus:outline-none'/>
+    <IoSearchSharp className='absolute bottom-3 left-3 text-gray-400 text-2xl'/>
     </div>
   )
 }
