1dcf887df3ce069c12e933715326b40920aa518d
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-09-21 12:32:14 +0100

    styles fix on country page

--- pages/country/[code].js
+++ pages/country/[code].js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react'
 import Layout from '../../components/Layout';
+import Link from 'next/link';
 
 export async function getServerSideProps( {params} ){
     const code = params.code;
@@ -20,18 +21,28 @@ function Country({ country }) {
   return (
     <div className='bg-gray-100 h-screen w-screen'>
     <Layout />
-    <div className='flex flex-row w-11/12 m-auto'>
-      <div>
-        <img src={country[0].flags.png}/>
+
+    <div className='w-11/12 mt-20 m-auto'>
+    <button className='bg-white px-7 py-1 rounded-lg shadow-md flex flex-row items-center font-light'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-5 h-5 mr-1">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+</svg>
+Back</button>
+    </div>
+
+    <div className='flex flex-col md:grid md:grid-cols-2 w-11/12 m-auto mt-20'>
+      
+      <div className='md:col-span-1 w-full'>
+        <img src={country[0].flags.png} className="w-full md:w-[85%] object-cover h-80"/>
       </div>
 
 {/**Right Box */}
-      <div className='h-full '>
-        <h3 className='font-bold text-xl'>{country[0].name.common}</h3>
+      <div className='flex flex-col justify-between'>
+        <h3 className='font-bold text-2xl'>{country[0].name.common}</h3>
         <div className='flex flex-col justify-between'>
-          <div className='flex flex-row'>
-            <span className='font-medium'>Native name: <span className='font-light ml-3'>{country[0].name.official}</span></span>
-            <span className='ml-5 font-medium'>Top level Domain:{' '} 
+          <div className='flex flex-row py-2'>
+            <span className='font-medium text-sm'>Native name: <span className='font-light ml-3'>{country[0].name.official}</span></span>
+            <span className='ml-10 font-medium text-sm'>Top level Domain:{' '} 
             <span className='font-light ml-3'>
             { country[0].tld.length > 1 ? country[0].tld.map(tld => (
               <span>{tld} </span>
@@ -39,30 +50,30 @@ function Country({ country }) {
             </span>
           </div>
 
-          <div className='flex flex-row'>
-            <span className='font-medium'>Population: <span className='font-light'>{country[0].population}</span></span>
-            <span className='ml-5 font-medium'>Currencies: 
+          <div className='flex flex-row py-2'>
+            <span className='font-medium text-sm'>Population: <span className='font-light'>{country[0].population}</span></span>
+            <span className='ml-10 font-medium text-sm'>Currencies: 
             <span className='font-light ml-3'>{Object.values(country[0].currencies)[0].name}</span>
             </span>
           </div>
 
-          <div className='flex flex-row'>
-            <span className='font-medium'>Region: <span className='font-light ml-3'>{country[0].region}</span></span>
-            <span className='ml-5 font-medium'>Languages: 
+          <div className='flex flex-row py-2'>
+            <span className='font-medium text-sm'>Region: <span className='font-light ml-3'>{country[0].region}</span></span>
+            <span className='ml-10 font-medium text-sm'>Languages: 
             <span className='font-light ml-3'>
             {Object.values(country[0].languages).length > 1 ? Object.values(country[0].languages).map(lan => <span className='ml-2'>{lan}</span>) : Object.values(country[0].languages)[0] }
               </span>
               </span>
           </div>
 
-          <div className='flex flex-row'>
-            <span className='font-medium'>Sub Region: <span className='font-light ml-3'>{country[0].subregion}</span></span>
-            <span className='ml-5 font-medium'>Capital: <span className='font-light ml-3'>{country[0].capital}</span></span>
+          <div className='flex flex-row py-2'>
+            <span className='font-medium text-sm'>Sub Region: <span className='font-light ml-3'>{country[0].subregion}</span></span>
+            <span className='ml-10 font-medium font-sm'>Capital: <span className='font-light ml-3'>{country[0].capital}</span></span>
           </div>
         </div>
 
         <div>
-          <span className='font-medium'>Border Countries: 
+          <span className='font-bold text-sm'>Border Countries: 
           {country[0].borders.length > 0 ? country[0].borders.map(border => 
           <span className='rounded-lg ml-3 shadow-md bg-white py-1 px-2 text-sm font-light'>{border}</span>
           ) : ''}
