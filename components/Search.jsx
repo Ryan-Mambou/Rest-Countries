f50b0592421d2a00ca80f7763753d8328593813e
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-10-10 02:33:02 +0100

    responsive

--- components/Search.jsx
+++ components/Search.jsx
@@ -5,7 +5,7 @@ function Search({handleChange}) {
 
   return (
     <div className='relative '>
-    <input placeholder='Search for a country...' onChange={handleChange}  className='text-sm py-3 md:w-[30rem]  rounded pl-10 shadow-lg focus:outline-none dark:bg-slate-800 dark:text-white'/>
+    <input placeholder='Search for a country...' onChange={handleChange}  className='text-sm py-3 md:w-[30rem] w-[15rem]  rounded pl-10 shadow-lg focus:outline-none dark:bg-slate-800 dark:text-white'/>
     <IoSearchSharp className='absolute bottom-3 left-3 text-gray-400 text-2xl dark:text-white'/>
     </div>
   )
