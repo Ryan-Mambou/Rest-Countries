7ad0e9f1f7922e3b2a5d16d1b6b7bf7134b4d3da
Ryan Djemtchemeu <ryanmambou@gmail.com>
2022-10-10 02:35:58 +0100

    responsive

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,10 +12,10 @@ function Layout() {
   return (
     <div className={`bg-white shadow-lg w-full dark:bg-slate-800 dark:text-white`}>
        <div className='flex flex-row w-11/12 m-auto justify-between items-center py-7'>
-        <h1 className='md:text-2xl text-lg font-bold'>Where in the world?</h1>
+        <h1 className='md:text-2xl text-xl font-bold'>Where in the world?</h1>
         <span>
-            {theme === 'light' ? <span onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className='flex flex-row items-center cursor-pointer'><IoMoon className='mr-2 text-2xl'/> <span className='font-medium'>Dark Mode</span></span>
-            : <span onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className='flex flex-row items-center cursor-pointer'><IoIosSunny className='mr-2 text-2xl'/> <span className='font-medium'>Light Mode</span></span>
+            {theme === 'light' ? <span onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className='flex flex-row items-center cursor-pointer'><IoMoon className='mr-2 text-2xl md:text-xl'/> <span className='font-medium'>Dark Mode</span></span>
+            : <span onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className='flex flex-row items-center cursor-pointer'><IoIosSunny className='mr-2 text-2xl md:text-xl'/> <span className='font-medium'>Light Mode</span></span>
             }
         </span>
         </div> 
