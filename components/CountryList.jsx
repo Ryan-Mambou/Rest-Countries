7e2f6e283a35261e4ba36ceb0421195a57fe8d4c
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-05-21 20:12:48 +0100

    fix: fix countrylist layout height to min 100vh to show flags upon search on mobile

--- components/CountryList.jsx
+++ components/CountryList.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import CountryCard from './CountryCard'
+import React from "react";
+import CountryCard from "./CountryCard";
 
-function CountryList({countries}) {
+function CountryList({ countries }) {
   return (
-    <div className='w-11/12 m-auto md:grid mt-10 gap-10 md:grid-cols-4 flex flex-col'>
-        {countries.map((country) => (
-            <CountryCard key={country.cca2} country={country}/>
-        ))}
+    <div className="w-11/12 m-auto md:grid mt-10 gap-10 md:grid-cols-4 flex flex-col min-h-screen">
+      {countries.map((country) => (
+        <CountryCard key={country.cca2} country={country} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default CountryList
+export default CountryList;
